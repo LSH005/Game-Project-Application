a3fb7c8602cec6d5611f95db0863b3ebf99e1cfa
LSH005 <isihyeon005shingu@gmail.com>
2025-09-09 15:01:11 +0900

    [feat] 플레이어 점프와 움직임 구현

--- Assets/Resources/Script/PlayerMovement.cs
+++ Assets/Resources/Script/PlayerMovement.cs
@@ -3,19 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpPower = 5f;
+    public float gravity = -9.81f;
 
     private CharacterController chrCont;
-    void Start()
+    private Vector3 velocity;
+    public bool isGrounded = true;
+
+    void Awake()
     {
         chrCont = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        isGrounded = chrCont.isGrounded;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(h, 0, v);
+        Vector3 move = new Vector3(x, 0, z);
         chrCont.Move(move * moveSpeed * Time.deltaTime);
+
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            velocity.y = jumpPower;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        chrCont.Move(velocity*Time.deltaTime);
     }
 }
